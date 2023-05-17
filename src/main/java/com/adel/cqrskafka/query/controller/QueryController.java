8e6e75e861f0bdea93fa0498374518c73d143496
adel5210 <adel.albediwy@gmail.com>
2023-05-17 15:04:46 +0800

    - Working simple publish/subscriber - Disable Security config

--- src/main/java/com/adel/cqrskafka/query/controller/QueryController.java
+++ src/main/java/com/adel/cqrskafka/query/controller/QueryController.java
@@ -4,6 +4,7 @@ import com.adel.cqrskafka.query.service.PublisherService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
